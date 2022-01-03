1ea184a35de2dc43b8bf523b5ae869d2ba2e4247
LuvBoy-76 <78361904+cjboy76@users.noreply.github.com>
2022-01-03 14:14:12 +0800

    Update allboard.spec.js

--- tests/unit/allboard.spec.js
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from "@vue/test-utils";
-import AllBoard from "@/views/AllBoard.vue";
-
-test("display test", () => {
-  const wrapper = shallowMount(AllBoard);
-  expect(wrapper.text()).toContain("所有看板");
-});
